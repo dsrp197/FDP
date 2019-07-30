8655ee0c9970a1cf9bda04de9e2e2b72e2e0e04a
dsrp197 <53283904+dsrp197@users.noreply.github.com>
2019-07-29 22:10:16 -0500

    Add files via upload

---
+++ Desafío 01 - Cálculo de Tarifa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de salario
+            Console.WriteLine("Ingrese su salario en cuestión de smmlvm, sabiendo que 1 smmlv = 828,116 COP");
+            int salario = int.Parse(Console.ReadLine());
+
+            //Condiciones de acuerdo a el número de smmlv que el usuario adquiere
+
+            if (salario < 2)
+            {
+                Console.WriteLine("Tarifa A");
+            }
+
+            else if (2 <= salario && salario < 4)
+            {
+                Console.WriteLine("Tarifa B");
+            }
+
+            else
+            {
+                Console.WriteLine("Tarifa C");
+            }
+        }
+    }
+}
---
+++ Desafío 02 - Cálculo cuota moderadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de salario
+            Console.WriteLine("Ingrese su salario en cuestión de smmlv. Teniendo en cuenta que 1 smmlv = 828,116 COP");
+            int salario = int.Parse(Console.ReadLine());
+
+            //Condiciones teniendo en cuenta el número de smmlv que gana la persona
+
+            if (salario < 2)
+            {
+                Console.WriteLine("Tarifa A");
+                Console.WriteLine("Cuota moderadora = 3.200");
+
+            }
+
+            else if (2 <= salario && salario < 5)
+            {
+                Console.WriteLine("Tarifa B");
+                Console.WriteLine("Cuota moderadora = 12.700");
+            }
+
+            else
+            {
+                Console.WriteLine("Tarifa C");
+                Console.WriteLine("Cuota moderadora = 33.500");
+            }
+        }
+    }
+}
