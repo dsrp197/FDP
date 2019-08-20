29eb64fdd678a6e4dee5ffaeb20333f3029743d6
dsrp197 <53283904+dsrp197@users.noreply.github.com>
2019-08-20 08:37:49 -0500

    Add files via upload

---
+++ C1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random aleatorio = new Random();
+            int dado = 0;
+            int total = 0;
+            int con = 0;
+            int vicon = 0;
+            string continuar = "s";
+
+
+            dado = aleatorio.Next(1, 13);
+            total += dado;
+            Console.WriteLine("Dado: " + dado);
+            Console.WriteLine("Total: " + total);
+            continuar = Console.ReadLine();
+            if (dado == 10)
+            {
+                con = con + 1;
+            }
+
+            if (continuar == "s")
+            {
+
+                dado = aleatorio.Next(1, 13);
+                total += dado;
+                if (dado == 12)
+                {
+                    con = con + 1;
+                }
+                if (dado != 10 && dado != 12)
+                {
+                    con = 0;
+                }
+                if (dado == 10 && con >= 1)
+                {
+                    Console.WriteLine("Dado " + dado);
+                    Console.WriteLine("Total: " + total);
+
+                    Console.WriteLine("Comodín! Ganaste por sacar 12 y 10 consecutivamente");
+                    vicon = vicon + 2;
+
+
+                }
+                if (vicon < 2)
+                {
+                    Console.WriteLine("Dado: " + dado);
+                    Console.WriteLine("Total: " + total);
+                    Console.WriteLine("Desea continuar? (s/n)");
+                    continuar = Console.ReadLine();
+                    if (continuar == "s")
+                    {
+
+                        dado = aleatorio.Next(1, 13);
+                        total += dado;
+                        if (dado == 10)
+                        {
+                            con = con + 1;
+                        }
+                        if (dado != 12 && dado != 10)
+                        {
+                            con = 0;
+                        }
+                        if (dado == 12 && con >= 1)
+                        {
+                            Console.WriteLine("Comodín! Ganaste");
+                            vicon = vicon + 2;
+                        }
+                        if (vicon < 2)
+                        {
+                            Console.WriteLine("Dado: " + dado);
+                            Console.WriteLine("Total " + total);
+                            Console.WriteLine("¿Deseas seguir?");
+                            continuar = Console.ReadLine();
+                        }
+                    }
+                }
+            }
+
+            dado = 2;
+
+            while (continuar == "s" && total < 100 && dado % 2 == 0 && vicon < 2)
+            {
+
+                dado = aleatorio.Next(1, 13);
+                Console.WriteLine("Dado: " + dado);
+                if (dado == 10)
+                {
+                    con = con + 1;
+                }
+                if (dado != 12 && dado != 10)
+                {
+                    con = 0;
+                }
+                if (dado == 12 && con >= 1)
+                {
+                    total += dado;
+                    Console.WriteLine("Comodín! Ganaste");
+                    vicon = vicon + 2;
+                }
+                if (dado % 2 != 0)
+                {
+                    total += dado;
+                    Console.WriteLine("Perdiste");
+                }
+                if (total >= 100)
+                {
+                    Console.WriteLine("Ganaste");
+                    total += dado;
+                }
+                if (dado % 2 == 0 && vicon < 2)
+                {
+                    total += dado;
+                    Console.WriteLine("Total: " + total);
+                    if (total < 100)
+                    {
+                        Console.WriteLine("¿Deseas seguir? (s/n)");
+                        continuar = Console.ReadLine();
+                    }
+                }
+            }
+            Console.WriteLine("Fin");
+            Console.WriteLine("Total: " + total);
+        }
+
+    }
+}
---
+++ D1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+
+            int dadouno = 0;
+            int dadodos = 0;
+            int total = 0;
+            string continuar = "s";
+            int contador = 0;
+            int mayor = 0;
+            int turnos = 0;
+
+            while (continuar == "s" && dadouno + dadodos != 2)
+            {
+                dadouno = aleatorio.Next(1, 7);
+                Console.WriteLine("Dado 1: " + dadouno);
+                dadodos = aleatorio.Next(1, 7);
+                Console.WriteLine("Dado 2: " + dadodos);
+                turnos += 1;
+
+
+
+                if (dadouno == 1 && dadodos == 1)
+                {
+                    total = 0;
+                    Console.WriteLine("Eliminado");
+
+                }
+
+                else
+                {
+                    total += dadouno + dadodos;
+                    Console.WriteLine("Total: " + total);
+                    if ((dadouno + dadodos) > 6)
+                    {
+                        mayor += 1;
+                    }
+
+                    if (total >= 100)
+                    {
+                        Console.WriteLine("Has ganado, felicidades");
+                        continuar = "";
+                    }
+                    else if (dadouno == dadodos)
+                    {
+                        contador += 1;
+
+                        if (contador == 3)
+                        {
+                            Console.WriteLine("Has ganado, felicidades");
+                            continuar = "";
+                        }
+
+                        Console.WriteLine("¿Deseas seguir?  (s/n) :");
+                        continuar = Console.ReadLine();
+                    }
+
+
+
+                    else if (dadouno != dadodos)
+                    {
+                        contador = 0;
+                        Console.WriteLine("¿Deseas seguir?  (s/n) :");
+                        continuar = Console.ReadLine();
+                    }
+
+
+                    else
+                    {
+                        Console.WriteLine("¿Deseas seguir?  (s/n) :");
+                        continuar = Console.ReadLine();
+                    }
+
+
+                }
+            }
+
+            mayor = (mayor * 100) / turnos;
+            Console.WriteLine("Su total fue de " + total + " puntos");
+            Console.WriteLine("Gracias por jugar");
+            Console.WriteLine("La suma de sus dados fue mayor a seis " + mayor + "% de las veces");
+
+
+        }
+    }
+}
